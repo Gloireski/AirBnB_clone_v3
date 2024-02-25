bcb19f2447625c0557674de5ee0415e26a51822d
Gloireski <belemgloirebekoutou8@gmail.com>
2024-02-25 01:29:57 +0000

    fix 3. Status of your API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ index """
 from api.v1.views import app_views
-#import jsonify
+from flask import jsonify
 
 
 @app_views.route('/status')
 def index():
     """ return status ok """
-    return {"status": "OK"}
+    return jsonify({"status": "OK"})
