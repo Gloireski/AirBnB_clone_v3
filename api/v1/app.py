85d2e11213c191b31d3d472f5525ba16a8923466
Gloireski <belemgloirebekoutou8@gmail.com>
2024-02-25 01:05:05 +0000

    3. Status of your API

---
+++ api/v1/app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" app """
+from flask import Flask, Blueprint
+from models import storage
+from api.v1.views import app_views
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def tearDowm(self):
+    """ close the storage engine"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    if os.getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = "0.0.0.0"
+    else:
+        HBNB_API_HOST = os.getenv("HBNB_API_HOST")
+    if os.getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = "5000"
+    else:
+        HBNB_API_PORT = os.getenv("HBNB_API_PORT")
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
